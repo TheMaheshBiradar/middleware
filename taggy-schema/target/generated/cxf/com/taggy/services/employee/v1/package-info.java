d4b7ac896bb19380a52fd85061aed9b8e61e4643
unknown <biradar.m2@gmail.com>
2015-04-06 21:09:01 +0530

    Removed Target folder

--- taggy-schema/target/generated/cxf/com/taggy/services/employee/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://com/taggy/services/employee/v1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.taggy.services.employee.v1;
